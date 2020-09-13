57dd07522cab05e78e24e5650acd94a9311631ba
Vyacheslav <vybochok@gmail.com>
2020-09-13 15:42:53 +0300

    Add end game method

--- game_server/room.py
+++ game_server/room.py
@@ -9,7 +9,15 @@ import asyncio
 from .player import Player, PlayerAction
 
 
+class EndGameException(Exception):
+    pass
+
+
 class GameRoom(metaclass=ABCMeta):
+    # How many players are needed to start the game.
+    ROOM_SIZE: int = 2
+    # How many iterations the game will take.
+    MAX_ITERATIONS = 100
 
     players: List[Player]
     is_finished: bool = False
@@ -31,40 +39,50 @@ class GameRoom(metaclass=ABCMeta):
         :param player_actions: list
         """
 
-    async def wait_for_user_input(self, player: Player) -> PlayerAction:
+    async def _wait_for_user_input(self, player: Player) -> PlayerAction:
         try:
             player_action_data = await asyncio.wait_for(player.connection.get_user_action(), timeout=1)
             return PlayerAction(player, player_action_data)
         except asyncio.TimeoutError as e:
             return PlayerAction(player, None)
 
-    async def get_players_actions(self) -> List[PlayerAction]:
+    async def _get_players_actions(self) -> List[PlayerAction]:
         await asyncio.wait([player.connection.send_state(self.state) for player in self.players])
-        return await asyncio.gather(*[self.wait_for_user_input(player) for player in self.players])
+        return await asyncio.gather(*[self._wait_for_user_input(player) for player in self.players])
+
+    async def end_game(self, reason: str) -> None:
+        """Should be called in `play` method to stop the game before all iterations will be passed"""
+        raise EndGameException(reason)
 
     async def start(self) -> None:
 
         actions_history = []
         states_history = []
 
+        end_game_message = "The game was finished."
+
         # main game cycle
-        for i in range(self.MAX_ITERATIONS):
-            players_actions = await self.get_players_actions()
+        try:
+            for i in range(self.MAX_ITERATIONS):
+                players_actions = await self._get_players_actions()
 
-            actions_history.append([dict(  # save copy to history
-                player_login=action.player.login,
-                data=dict(action.data)
-            ) for action in players_actions])
+                actions_history.append([dict(  # save copy to history
+                    player_login=action.player.login,
+                    data=dict(action.data)
+                ) for action in players_actions])
 
-            self.play(players_actions)
+                self.play(players_actions)
 
-            states_history.append(dict(self.state))    # save copy to history
+                states_history.append(dict(self.state))    # save copy to history
+        except EndGameException as e:
+            states_history.append(dict(self.state))
+            end_game_message += str(e)
 
         # on finish
         for player in self.players:
-            await player.connection.close(1000, "The game was finished.")
+            await player.connection.close(1000, end_game_message)
 
-        print(f"The game was finished with status: {self.state}")
+        print(f"The game was finished with state: {self.state}")
 
         if not os.path.exists('results'):
             os.makedirs('results')
